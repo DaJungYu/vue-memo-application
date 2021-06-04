0425e1f289e7e2fe56fd6673b795c0d2e928d430
DaJungYu <tender5688@gmail.com>
2021-06-04 17:53:11 +0900

    REST API

--- src/components/MemoApp.vue
+++ src/components/MemoApp.vue
@@ -9,8 +9,13 @@
 </template>
 
 <script>
+import axios from 'axios';
 import MemoForm from './MemoForm';
 import Memo from './Memo';
+
+const memoAPIcore =axios.create({
+    baseURL: 'http://localhost:8000/api/memos'
+});
 
 export default {
     name: 'MemoApp',
@@ -20,13 +25,21 @@ export default {
     },
     methods:{
         addMemo(payload){
-            this.memos.push(payload);
-            this.storeMemo();
+            //this.memos.push(payload);
+            //this.storeMemo();
+            memoAPIcore.post('/',payload)
+            .then(res => {
+                this.memos.push(res,data)
+            });
         },
         deleteMemo(id) { //자식 컴포넌트에서 인자로 전달해주는 id에 해당하는 메모 데이터의 인덱스 찾기
             const targetIndex = this.memos.findIndex(v => v.id === id); // 찾은 인덱스 번호에 해당하는 메모데이터 삭제
-            this.memos.splice(targetIndex,1);//삭제된 후 데이터를 다시 로컬 스토리지에 마찬가지로 저장.
-            this.storeMemo();
+            memoAPIcore.delete(`/${id}`)
+                .then(()=> {
+                this.memos.splice(targetIndex,1);
+            });
+            //this.memos.splice(targetIndex,1);//삭제된 후 데이터를 다시 로컬 스토리지에 마찬가지로 저장.
+            //this.storeMemo();
         },
         storeMemo() {
             const memosToString =JSON.stringify(this.memos);
@@ -36,8 +49,19 @@ export default {
             const {id,content}=payload;
             const targetIndex=this.memos.findIndex(v=>v.id===id);
             const targetMemo=this.memos[targetIndex];
-            this.memos.splice(targetIndex,1,{...targetMemo,content});
-            this.storeMemo();
+
+            memoAPIcore.put(`/${id}`,{content})
+                .then(()=> {
+                    this.memos.splice(targetIndex,1,{...targetMemo,content});
+                });
+            //this.memos.splice(targetIndex,1,{...targetMemo,content});
+            //this.storeMemo();
+        },
+        created() {
+            memoAPIcore.get('/')
+            .then(res => {
+                this.memos = res.data;
+            });
         }
     },
     data() {
